8a408c623cd9b6a62b626ec343a5cae4f80c2161
Skycat1983 <hlaoutaris@gmail.com>
2024-08-12 21:18:24 +0200

    Update styling in page.tsx and Collection.tsx

--- src/app/artwork/[slug]/page.tsx
+++ src/app/artwork/[slug]/page.tsx
@@ -8,7 +8,7 @@ export default async function Item({ params }: { params: { slug: string } }) {
   console.log("sectionItem :>> ", sectionItem);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-4">
+    <main className="flex min-h-screen flex-col items-center justify-between px-8 lg:px-24 py-4">
       {sectionItem && <Collection collection={sectionItem} />}
     </main>
   );
--- src/views/Collection.tsx
+++ src/views/Collection.tsx
@@ -6,7 +6,7 @@ import ArtworkInfoCard from "@/components/cards/artworkInfoCard/ArtworkInfoCard"
 import Pagination from "@/components/ui/pagination/Pagination";
 import { IFrontendArtwork } from "@/lib/types/artworkTypes";
 import Image from "next/image";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 
 interface CollectionProps {
   collection: {
@@ -37,13 +37,13 @@ const Collection: React.FC<CollectionProps> = ({ collection }) => {
   );
 
   console.warn(collection);
-
   return (
-    <div className="flex flex-col justify-start items-center w-auto  max-w-[90vw] bg-black/10">
-      <div className="container bg-blue-100  w-full grid gap-10 lg:grid-cols-2 lg:gap-0 ">
+    <div className="container mx-auto flex flex-col justify-start w-full  bg-black/10">
+      <div className=" bg-blue-100 w-full grid gap-10 lg:grid-cols-2 lg:gap-0 ">
         {displayedArtwork && (
           <>
-            <div className="flex flex-row justify-center align-start h-auto max-h-[75vh]  lg:justify-end  overflow-none bg-red-100">
+            {/* image */}
+            <div className="flex flex-row  align-start h-auto max-h-[75vh] justify-center lg:justify-end  overflow-none bg-red-100">
               <div className="flex flex-col bg-green-100">
                 <Image
                   src={displayedArtwork.image.secure_url}
@@ -54,7 +54,8 @@ const Collection: React.FC<CollectionProps> = ({ collection }) => {
                 />
               </div>
             </div>
-            <div className="flex flex-row justify-center w-2/3 lg:w-full lg:justify-end h-auto overflow-none bg-blue-100">
+            {/* card */}
+            <div className="mx-auto flex justify-center w-2/3 lg:w-full lg:justify-end h-auto overflow-none bg-blue-100">
               <ArtworkInfoCard {...displayedArtwork} />
             </div>
           </>
@@ -84,10 +85,49 @@ const Collection: React.FC<CollectionProps> = ({ collection }) => {
 
 export default Collection;
 
-{
-  /* <ArtworkImage
-              src={displayedArtwork.image.secure_url}
-              alt="Artwork Title"
-              isShowing={isShowing}
-            /> */
-}
+// !
+//   return (
+//     <div className="flex flex-col justify-start items-center w-auto  max-w-[90vw] bg-black/10">
+//       <div className="container bg-blue-100  w-full grid gap-10 lg:grid-cols-2 lg:gap-0 ">
+//         {displayedArtwork && (
+//           <>
+//             <div className="flex flex-row justify-center align-start h-auto max-h-[75vh]  lg:justify-end  overflow-none bg-red-100">
+//               <div className="flex flex-col bg-green-100">
+//                 <Image
+//                   src={displayedArtwork.image.secure_url}
+//                   alt="Artwork Title"
+//                   height={displayedArtwork.image.pixelHeight}
+//                   width={displayedArtwork.image.pixelWidth}
+//                   className="max-h-full w-auto max-w-[90vw] object-contain self-start"
+//                 />
+//               </div>
+//             </div>
+//             <div className="flex flex-row justify-center w-2/3 lg:w-full lg:justify-end h-auto overflow-none bg-blue-100">
+//               <ArtworkInfoCard {...displayedArtwork} />
+//             </div>
+//           </>
+//         )}
+//       </div>
+//       <div className="pb-8">
+//         <HorizontalDivider />
+//       </div>
+//       {collection.artworks.length > 1 && (
+//         <div className="flex flex-row w-full justify-center py-8 h-[400px]">
+//           <Pagination
+//             totalPages={collection.artworks.length}
+//             handlePageChange={handlePageChange}
+//             pageRangeDisplayed={collection.artworks.length}
+//             showFirstLast={false}
+//             showPrevNext={true}
+//             paginationItems={collection.artworks}
+//           />
+//         </div>
+//       )}
+//       <div className="py-8">
+//         <HorizontalDivider />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Collection;
