0ac4dec2989ecaf7eaa8ac170ba01b14d20ad287
Skycat1983 <hlaoutaris@gmail.com>
2024-08-23 18:47:11 +0200

    Refactor collection layout and add artist profile

--- src/app/artwork/[collectionSlug]/layout.tsx
+++ src/app/artwork/[collectionSlug]/layout.tsx
@@ -38,25 +38,33 @@ export default async function CollectionLayout({
       <div className="px-4 py-8">
         <HorizontalDivider />
       </div>
-      <h1 className="px-4 py-6 text-2xl font-bold">About this collection</h1>
-      <p className="px-4 text-primary">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quo eius
-        ipsa exercitationem deleniti eligendi sint nisi consequatur quaerat ut.
-        Nesciunt quaerat aliquam nobis alias libero repellendus ducimus. Ea
-        dolores aliquam soluta dolorem voluptatibus quasi impedit minus, beatae
-        quaerat id dignissimos veritatis, nemo laborum, vel molestiae et fuga
-        libero ab?
-      </p>
-      <div className="px-4 py-8">
-        <HorizontalDivider />
+      <div className="flex flex-col w-full md:flex-row ">
+        <div className="flex flex-col">
+          <h1 className="px-4 py-6 text-2xl font-bold">
+            About this collection
+          </h1>
+          <p className="px-4 text-primary">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quo
+            eius ipsa exercitationem deleniti eligendi sint nisi consequatur
+            quaerat ut. Nesciunt quaerat aliquam nobis alias libero repellendus
+            ducimus. Ea dolores aliquam soluta dolorem voluptatibus quasi
+            impedit minus, beatae quaerat id dignissimos veritatis, nemo
+            laborum, vel molestiae et fuga libero ab?
+          </p>
+        </div>
+
+        <div className="px-4 py-8 md:hidden">
+          <HorizontalDivider />
+        </div>
+        <div className="bg-slate-800/10 w-full flex flex-col w-full">
+          <ArtistProfile />
+        </div>
       </div>
-      <div className="bg-slate-800/10 w-full">
-        <ArtistProfile />
-      </div>
+
       <div className="px-4 py-4">
         <HorizontalDivider />
       </div>
-      <div className="px-4">
+      <div className="px-4 w-full md:w-1/2 mx-auto">
         <h1 className=" py-6 text-2xl font-bold">Subscribe for updates</h1>
         {/* <HorizontalDivider /> */}
         <SubscribeForm />
