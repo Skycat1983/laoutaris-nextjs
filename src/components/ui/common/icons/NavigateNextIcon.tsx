44df0e17df827b2d935ee19e9f0d6456c5309e49
Skycat1983 <hlaoutaris@gmail.com>
2025-02-04 17:09:00 +0100

    Add NavigateNextIcon component and update BlogEntriesView

---
+++ src/components/ui/common/icons/NavigateNextIcon.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+
+export function NavigateNextIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="100%"
+      height="100%"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M10.02 6L8.61 7.41L13.19 12l-4.58 4.59L10.02 18l6-6z"
+      ></path>
+    </svg>
+  );
+}
--- src/components/views/BlogEntriesView.tsx
+++ src/components/views/BlogEntriesView.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import { FrontendBlogEntryUnpopulated } from "@/lib/types/blogTypes";
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "../ui/shadcn/button";
+import { NavigateNextIcon } from "../ui/common/icons/NavigateNextIcon";
 
 type BlogPageLink = string | null;
 
@@ -96,17 +98,31 @@ const BlogEntriesView = ({
         <div className="col-span-1 xl:col-span-2"></div>
       </div>
       <div className="flex justify-center mt-4">
-        {prev && (
-          <Link href={prev} className="px-4 py-2 bg-gray-200 rounded">
-            Previous
-          </Link>
-        )}
+        <div className="flex flex-row w-[200px] justify-center items-center gap-5">
+          {prev ? (
+            <Link href={prev}>
+              <div className="text-black rounded-full border border-1 border-black w-12 h-12 flex justify-center items-center hover:bg-gray-200">
+                <NavigateNextIcon style={{ transform: "rotate(180deg)" }} />
+              </div>
+            </Link>
+          ) : (
+            <div className="bg-gray-300 text-gray-500 rounded-full border border-1 border-gray-500 w-12 h-12 flex justify-center items-center cursor-not-allowed">
+              <NavigateNextIcon style={{ transform: "rotate(180deg)" }} />
+            </div>
+          )}
 
-        {next && (
-          <Link href={next} className="px-4 py-2 bg-gray-200 rounded">
-            Next
-          </Link>
-        )}
+          {next ? (
+            <Link href={next}>
+              <div className="text-black rounded-full border border-1 border-black w-12 h-12 flex justify-center items-center hover:bg-gray-200">
+                <NavigateNextIcon />
+              </div>
+            </Link>
+          ) : (
+            <div className="bg-gray-300 text-gray-500 rounded-full border border-1 border-gray-500 w-12 h-12 flex justify-center items-center cursor-not-allowed">
+              <NavigateNextIcon />
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
