b21bcb763a5b6956ee4a6ad74861a87ed3d94fac
Skycat1983 <hlaoutaris@gmail.com>
2025-02-26 20:02:36 +0100

    Enhance Blog Detail Page with Skeleton Loading and Improved Import Structure
    - Updated BlogSlug page to use BlogDetailSkeleton for more informative loading state
    - Refined import paths for BlogDetailLoader and added new skeleton imports
    - Improved loading experience with a dedicated skeleton component

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
-import { BlogDetailLoader } from "@/components/loaders/viewLoaders";
+import { BlogDetailLoader } from "@/components/loaders/viewLoaders/BlogDetailLoader";
 import { Suspense } from "react";
-
+import { BlogDetailSkeleton } from "@/components/views/BlogDetail";
+import ArticleViewSkeleton from "@/components/elements/skeletons/ArticleViewSkeleton";
 export default async function BlogSlug({
   params,
   searchParams,
@@ -12,7 +13,7 @@ export default async function BlogSlug({
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-12 py-4 container mx-auto">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<BlogDetailSkeleton />}>
         <BlogDetailLoader slug={params.slug} showComments={showComments} />
       </Suspense>
     </main>
