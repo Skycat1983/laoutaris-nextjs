df8be58926d8b08051400ff29f178067eb4b257c
Skycat1983 <hlaoutaris@gmail.com>
2025-03-19 11:45:44 +0100

    Update Project Redirect URL to Use VERCEL_URL or Localhost
    - Modified the redirect logic in the Project component to utilize VERCEL_URL for production environments, falling back to localhost for local development.
    - This change ensures consistent URL handling across different environments, improving navigation reliability.

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,7 +1,8 @@
 import { redirect } from "next/navigation";
 
 export default function Project() {
-  redirect(`${process.env.NEXTAUTH_URL}/project/about`);
+  const baseUrl = process.env.VERCEL_URL || "http://localhost:3000";
+  redirect(`${baseUrl}/project/about`);
   // return (
   //   <main className="flex  flex-col items-start justify-between px-24">
   //     {/*  */}
