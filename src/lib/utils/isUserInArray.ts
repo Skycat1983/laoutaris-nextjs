87e914fcc5860555fdf1d6b844a2e24a5b57ac67
Skycat1983 <hlaoutaris@gmail.com>
2025-04-04 11:56:27 +0200

    delete unused files

--- src/lib/utils/isUserInArray.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectId } from "mongoose";
-
-export const isUserInArray = (
-  array: (string | ObjectId)[],
-  userId: string | null | undefined
-): boolean => {
-  if (!userId || !array) return false;
-  return array.some((id) => id.toString() === userId);
-};
